bc81f0ab6689f1d041db472392f834d961832ff1
Polik32 <polik19195@gmail.com>
2022-01-11 22:17:05 +0200

    home 11.01.2022

--- app/js/main.js
+++ app/js/main.js
@@ -6,4 +6,15 @@ $('.products__slider').slick({
     infinite: false,
 });
 
+$('.questions__item-title').on('click', function(){
+    $('.questions__item').removeClass('questions__item--active');
+    $(this).parent().addClass('questions__item--active');
+});
+
+$('#fullpage').fullpage({
+    autoScrolling: true,
+    scrollHorizontally: true,
+    sectionSelector: '.page-section'
+})
+
 });
