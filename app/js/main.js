8d0268801fdadbf1959a44871bd2957421d4dde9
Polik32 <polik19195@gmail.com>
2022-01-12 21:48:12 +0200

    12.01.2022 home

--- app/js/main.js
+++ app/js/main.js
@@ -14,7 +14,14 @@ $('.questions__item-title').on('click', function(){
 $('#fullpage').fullpage({
     autoScrolling: true,
     scrollHorizontally: true,
-    sectionSelector: '.page-section'
+    sectionSelector: '.page-section',
+    scrollOverflow: true,
+    menu: '#header__nav',
+    anchors: ['top', 'products', 'benefits', 'specification', 'questions', 'contacts'],
+})
+
+$('.menu__btn').on('click', function(){
+    $('.menu__btn').toggleClass('menu__btn--active');
 })
 
 });
